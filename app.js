26e579b19e1ac4dccd2ec287b147c11fb2dbc411
DevSec <arma.devsec@gmail.com>
2018-05-16 14:27:07 +0200

    + log to file

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+const fs = require('fs')
+const Log = require('log')
+const path = require('path')
 const config = require('./config')
+
 const discord = require('./modules/discordbot').init(config)
 
 // require('./modules/bot').init(discord)
 require('./modules/prebot').init(discord)
 require('./modules/rssbot').init(discord)
+
+const log = new Log('info', fs.createWriteStream('./node.log'))
+global.consolelog = console.log
+let handleLog = (...args) => {
+  global.consolelog(...args)
+  log.info(...args)
+}
+console.log = handleLog
+console.error = handleLog
+
+let obj = {a: 1}
+obj.obj = obj
+console.log(obj)
